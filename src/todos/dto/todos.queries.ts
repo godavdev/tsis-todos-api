34473a2f36e0ad9e136365fef823914bf0da15cc
godavdev <godavdeveloper@gmail.com>
2026-03-06 11:16:10 -0600

    feat: enhance todos API with query transformation and logging

--- src/todos/dto/todos.queries.ts
+++ src/todos/dto/todos.queries.ts
@@ -1,7 +1,12 @@
+import { Transform } from 'class-transformer';
 import { IsBoolean, IsOptional } from 'class-validator';
 
 export class ListTodosQuery {
   @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    return false;
+  })
   @IsBoolean()
   completed?: boolean;
 }
